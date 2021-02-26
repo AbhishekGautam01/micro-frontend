cbe721b0a9dec3c5cdcb2d6e58fa62ef93470ef0
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-02-26 05:35:06 +0530

    Adding webpack config

---
+++ auth/config/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+//Since dev config is the second param - so this will take precedence
+// any thing which is common in both so devConfig will override
+module.exports = merge(commonConfig, devConfig);
